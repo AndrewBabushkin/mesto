20baf38d0240007940808e116d93620dfa3fdc97
Andrew Babushkin <andrewb1@yandex.ru>
2022-11-02 21:34:05 +0700

    feat: отправить проект на ревью

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+// Открытие и закрытие попапа.
+const editElem = document.querySelector(".profile__button_type_edit");
+const popupElem = document.querySelector(".popup");
+
+const onOpen = function (popup) {
+  popup.classList.add("popup_opened");
+};
+
+const onClose = function (popup) {
+  popup.classList.remove("popup_opened");
+};
+
+editElem.addEventListener("click", function () {
+  onOpen(popupElem);
+});
+
+popupElem.addEventListener("click", function (event) {
+  const isOverlay = event.target.classList.contains("popup_opened");
+  const isClose = event.target.classList.contains("popup__close");
+  if (isOverlay || isClose) {
+    onClose(popupElem);
+  }
+});
+
+// Редактирование информации и себе.
+
+let formElement = document.querySelector(".popup__content");
+let nameInput = formElement.querySelector(".popup__text_name");
+let professionInput = formElement.querySelector(".popup__text_profession");
+let nameProfile = document.querySelector(".profile__name");
+let professionProfile = document.querySelector(".profile__profession");
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameInput.value;
+  professionInput.value;
+
+  nameProfile.textContent = nameInput.value;
+  professionProfile.textContent = professionInput.value;
+
+  onClose(popupElem);
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
