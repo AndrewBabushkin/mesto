2547ad69727fc35d2e2ce03ef34be8485add09f7
Andrew Babushkin <andrewb1@yandex.ru>
2022-11-08 22:21:21 +0700

    fix: исправить стили, разметку и закрытите попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,27 @@
-// Открытие и закрытие попапа.
 const editElem = document.querySelector(".profile__button_type_edit");
 const popupElem = document.querySelector(".popup");
+const closeEdit = popupElem.querySelector(".popup__close");
+
+const formElement = document.querySelector(".popup__form-edit");
+let nameInput = formElement.querySelector(".popup__input-field_name_edit");
+let professionInput = formElement.querySelector(
+  ".popup__input-field_profession_edit"
+);
+let nameProfile = document.querySelector(".profile__name");
+let professionProfile = document.querySelector(".profile__profession");
 
 const onOpen = function (popup) {
   popup.classList.add("popup_opened");
+  nameInput.value = nameProfile.textContent;
+  professionInput.value = professionProfile.textContent;
 };
 
 const onClose = function (popup) {
   popup.classList.remove("popup_opened");
 };
 
-editElem.addEventListener("click", function () {
-  onOpen(popupElem);
-});
-
-popupElem.addEventListener("click", function (event) {
-  const isOverlay = event.target.classList.contains("popup_opened");
-  const isClose = event.target.classList.contains("popup__close");
-  if (isOverlay || isClose) {
-    onClose(popupElem);
-  }
-});
-
-// Редактирование информации и себе.
-
-let formElement = document.querySelector(".popup__form-edit");
-let nameInput = formElement.querySelector(".popup__text_name_edit");
-let professionInput = formElement.querySelector(".popup__text_profession_edit");
-let nameProfile = document.querySelector(".profile__name");
-let professionProfile = document.querySelector(".profile__profession");
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput.value;
-  professionInput.value;
 
   nameProfile.textContent = nameInput.value;
   professionProfile.textContent = professionInput.value;
@@ -42,3 +30,11 @@ function formSubmitHandler(evt) {
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
+
+editElem.addEventListener("click", function () {
+  onOpen(popupElem);
+});
+
+closeEdit.addEventListener("click", function () {
+  onClose(popupElem);
+});
